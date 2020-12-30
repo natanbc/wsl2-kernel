cfefd8ddb47b906d42bdb95464febd3f94ac25f6
natanbc <natanbc@users.noreply.github.com>
2020-12-29 22:30:10 -0300

    new stuff

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 - Clone the source: https://github.com/Microsoft/WSL2-Linux-Kernel
 - Edit the config (`make CC=cc HOSTCC=cc KCONFIG_CONFIG=Microsoft/config-wsl menuconfig`)
 - Build the kernel (`make CC=cc HOSTCC=cc KCONFIG_CONFIG=Microsoft/config-wsl -j$(nproc)`)
-- Run `./headers.sh path/to/clone path/to/headers/install/dir`
+- Run `./headers.sh path/to/clone [path/to/headers/install/dir]`
 
 
--- headers.sh
+++ headers.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 path/to/kernel path/to/headers/install/dir" 1>&2;
+die() {
+    echo "$@" 1>&2;
     exit 1;
+}
+
+if [ $# -lt 1 ]; then
+    die "Usage: $0 path/to/kernel [path/to/headers/install/dir]" 1>&2;
 fi
 
 ME=$(dirname $(readlink -f $0))
-D=$(mktemp -d)
-cp "$1/Module.symvers" "$D/"
-cp "$1/Microsoft/config-wsl" "$D/.config"
-"$ME/gen_mod_headers" "$2" "$1" "$D"
-rm -rf $D
+OBJ=$(mktemp -d)
+CONFIG=Microsoft/config-wsl
+SRC="$1"
+
+[ -d "$SRC" ]                || die "Unable to find kernel source"
+[ -r "$SRC/Module.symvers" ] || die "Please build the kernel"
+[ -r "$SRC/$CONFIG" ]        || die "Unable to find config file"
+
+DST=${2:-$(make -C "$SRC" CC=cc HOSTCC=cc -s kernelrelease LOCALVERSION=)}
+
+cp "$SRC/Module.symvers" "$OBJ/"
+cp "$SRC/$CONFIG"        "$OBJ/.config"
+"$ME/gen_mod_headers" "$DST" "$SRC" "$OBJ"
+rm -rf $OBJ
