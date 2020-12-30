463e8ffc7ac4a6c5286a88bda7d130a38898f9d2
natanbc <natanbc@users.noreply.github.com>
2020-12-29 22:37:14 -0300

    fix stuff

--- headers.sh
+++ headers.sh
@@ -5,22 +5,35 @@ die() {
     exit 1;
 }
 
-if [ $# -lt 1 ]; then
-    die "Usage: $0 path/to/kernel [path/to/headers/install/dir]" 1>&2;
+if [ $# -lt 2 ]; then
+    die "Usage: $0 path/to/built/kernel path/to/clean/kernel [path/to/headers/install/dir]" 1>&2;
 fi
 
 ME=$(dirname $(readlink -f $0))
 OBJ=$(mktemp -d)
+OUT=$(mktemp -d)
 CONFIG=Microsoft/config-wsl
-SRC="$1"
 
-[ -d "$SRC" ]                || die "Unable to find kernel source"
-[ -r "$SRC/Module.symvers" ] || die "Please build the kernel"
-[ -r "$SRC/$CONFIG" ]        || die "Unable to find config file"
+BUILT="$1"
+[ -d "$BUILT" ]                || die "Unable to find built kernel"
+[ -r "$BUILT/Module.symvers" ] || die "Please build the kernel"
+[ -r "$BUILT/$CONFIG" ]        || die "Unable to find config file"
 
-DST=${2:-$(make -C "$SRC" CC=cc HOSTCC=cc -s kernelrelease LOCALVERSION=)}
+SRC="$2"
+[ -d "$SRC" ] || die "Unable to find clean kernel"
 
-cp "$SRC/Module.symvers" "$OBJ/"
-cp "$SRC/$CONFIG"        "$OBJ/.config"
-"$ME/gen_mod_headers" "$DST" "$SRC" "$OBJ"
-rm -rf $OBJ
+DST=${3:-/lib/modules/$(make -C "$SRC" CC=cc HOSTCC=cc -s kernelrelease LOCALVERSION=)/build}
+
+echo "BUILT=$BUILT"
+echo "SRC=  $SRC"
+echo "DST=  $DST"
+#exit 0
+
+cp "$BUILT/Module.symvers" "$OBJ/"
+cp "$BUILT/$CONFIG"        "$OBJ/.config"
+"$ME/gen_mod_headers" "$OUT" "$SRC" "$OBJ"
+
+mkdir -p "$(dirname "$DST")"
+mv "$OUT" "$DST"
+
+rm -rf "$OBJ" "$OUT"
