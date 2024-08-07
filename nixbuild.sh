a30d500307d502f2fd253c765107b51ce06872e7
natanbc <natanbc@users.noreply.github.com>
2024-08-07 16:01:09 +0000

    Add script for pulling in build dependencies with nix

---
+++ nixbuild.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env nix-shell
+#! nix-shell --pure -p autoconf automake bash bc llvmPackages_17.bintools bison cacert llvmPackages_17.clangUseLLVM cpio curl elfutils flex git gnumake kmod libtool ncurses openssl perl util-linux -i sh
+ME="$(dirname -- "$(readlink -f -- "$0")")"
+exec "$ME/build.sh"
+
